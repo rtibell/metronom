5af6fcb12a572bcf3366b6423d97efc4730c009d
Rasmus Tibell <rasmus.tibell@gmail.com>
2018-12-01 20:31:35 +0100

    First commit

---
+++ src/main/java/com/tibell/metronom/App.java
@@ -0,0 +1,13 @@
+package com.tibell.metronom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

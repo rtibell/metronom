13d7cedd21ae79f0666f6109a7f021e2bcbf929b
Rasmus Tibell <rasmus.tibell@gmail.com>
2018-12-02 20:14:27 +0000

    added connection document

--- src/main/java/com/tibell/metronom/MetronomTimer.java
+++ src/main/java/com/tibell/metronom/MetronomTimer.java
@@ -6,11 +6,13 @@ import java.util.TimerTask;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 
 public class MetronomTimer {
 	 private static int NUMBER_OF_LEDS = 8;
+	 private static int NUMBER_OF_BUTTONS = 4;
 	
 	Timer timer;
 
@@ -18,12 +20,13 @@ public class MetronomTimer {
         final GpioController gpio = GpioFactory.getInstance();
 
 	GpioPinDigitalOutput led[];
+	GpioPinDigitalInput button[];
 	
 	public MetronomTimer(int bpm) {
             init();
             int delay = (60 * 1000) / bpm;
             timer = new Timer();
-            timer.scheduleAtFixedRate(new MetronomTask(delay/8), 2000, delay);
+            timer.scheduleAtFixedRate(new MetronomTask(delay/8, true), 2000, delay);
 	}
 	
 	private void init() {
@@ -33,6 +36,16 @@ public class MetronomTimer {
             led[1] = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "2a", PinState.HIGH);
             led[2] = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "3a", PinState.HIGH);
             led[3] = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "4a", PinState.HIGH);
+            led[4] = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "1b", PinState.HIGH);
+            led[5] = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "2b", PinState.HIGH);
+            led[6] = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_15, "3b", PinState.HIGH);
+            led[7] = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_16, "4b", PinState.HIGH);
+            
+            button = new GpioPinDigitalInput[NUMBER_OF_BUTTONS];
+	    button[0] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_12, "but-1");
+	    button[1] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_13, "but-2");
+	    button[2] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_14, "but-3");
+	    button[3] = gpio.provisionDigitalInputPin(RaspiPin.GPIO_07, "but-4");
 
             // set shutdown state for this pin
             for (int idx = 0; idx < NUMBER_OF_LEDS; idx++)
@@ -44,9 +57,11 @@ public class MetronomTimer {
 	public class MetronomTask extends TimerTask {
 		private int pct;
 		private int idx;
+		private boolean oneeight = false;
 
-		public MetronomTask(int pct) {
+		public MetronomTask(int pct, boolean oe) {
 			this.pct = pct;
+			this.oneeight = oe;
 			this.idx = 0;
 			System.out.println("Constructor for MetronomTask.");
 		}
@@ -55,12 +70,14 @@ public class MetronomTimer {
 		public void run() {
 			
 			try {
+				int i = ((idx % 2) == 0) ? (idx/2) : (idx/2)+4;
 				System.out.println("On-" + idx + "                   " + System.currentTimeMillis());
-				led[idx].high();
+				led[i].high();
 				Thread.sleep(this.pct);
 				System.out.println("Off-" + idx + " " + System.currentTimeMillis());
-				led[idx].low();
-                                idx = (idx+1) % 4;
+				led[i].low();
+                                idx = (idx+1) % 8;
+                                System.out.println("B1=" + button[0].isHigh() + " B2=" +  button[1].isHigh() + " B3=" +  button[2].isHigh() + " B4=" +  button[3].isHigh());
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -72,7 +89,7 @@ public class MetronomTimer {
 	
 	
 	public static void main(String[] args) {
-		MetronomTimer tt = new MetronomTimer(120);
+		MetronomTimer tt = new MetronomTimer(2*120);
 		
 	}
 
